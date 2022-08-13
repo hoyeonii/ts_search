dd81a2cfb2ade78a62352735a56f08bbdff5b99d
Hoyeon Lee <vnzk0301@naver.com>
2022-08-13 21:42:24 +0200

    sortBy added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import {
   Slider,
@@ -13,6 +12,11 @@ import {
   Autocomplete,
   TextField,
   ButtonGroup,
+  FormControl,
+  MenuItem,
+  InputLabel,
+  Select,
+  SelectChangeEvent,
 } from "@mui/material";
 function App() {
   interface IProduct {
@@ -26,11 +30,13 @@ function App() {
     stock: number;
     thumbnail: string;
     title: string;
+    discountPercentage: number;
   }
   const [data, setData] = useState<IProduct[]>([]);
   const [filteredData, setFilteredData] = useState<IProduct[]>([]);
   const [brand, setBrand] = useState<string[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [sortBy, setSortBy] = useState<string>("Rating");
   const [filterBrand, setFilterBrand] = useState<string | null>("All Brands");
   const [value, setValue] = useState<number[]>([30, 1500]);
   const [page, setPage] = useState<number>(1);
@@ -40,6 +46,7 @@ function App() {
       .then((res) => res.json())
       .then((json) => {
         setData(json.products);
+        console.log(json.products[0]);
         setBrand(
           json.products
             .map((product: IProduct) => product.brand)
@@ -47,6 +54,7 @@ function App() {
         );
       });
   }, []);
+
   useEffect(() => {
     setFilteredData(
       data
@@ -61,40 +69,71 @@ function App() {
             : true
         )
         .filter((el) => el.price > value[0] && el.price < value[1])
+        .sort((a, b) => {
+          switch (sortBy) {
+            case "Rating":
+              return b.rating - a.rating;
+              break;
+            case "Deals":
+              return b.discountPercentage - a.discountPercentage;
+              break;
+            case "Lowest":
+              return a.price - b.price;
+              break;
+            case "Highest":
+              return b.price - a.price;
+              break;
+            default:
+              return b.price - a.price;
+          }
+        })
     );
     setPage(1);
-  }, [data, query, filterBrand, value]);
+  }, [data, query, filterBrand, value, sortBy]);
 
-  console.log(Math.ceil(12 / (window.innerWidth / 300)));
   const handleChange = (e: Event, newValue: number | number[]) => {
     setValue(newValue as number[]);
   };
+  const handleSelectChange = (event: SelectChangeEvent) => {
+    console.log(event.target.value);
+    setSortBy(event.target.value);
+  };
 
   return (
-    <div className="App">
-      {/* <select
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-          setFilterBrand(e.target.value);
-        }}
-      >
-        <option>All Brands</option>
-        {brand.map((el) => (
-          <option>{el}</option>
-        ))}
-      </select> */}
-      <Autocomplete
-        value={filterBrand}
-        onChange={(e: any, value: string | null) => {
-          setFilterBrand(value);
-        }}
-        disablePortal
-        id="combo-box-demo"
-        options={["All Brands", ...brand]}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Brand" />}
-      />
-      {filterBrand}
-      <div style={{ width: "30%" }}>
+    <div className="App" style={{ margin: "3%" }}>
+      <div className="filterContainer" style={{ display: "flex" }}>
+        <input
+          type="text"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setQuery(e.target.value);
+          }}
+        />
+        <Autocomplete
+          value={filterBrand}
+          onChange={(e: any, value: string | null) => {
+            setFilterBrand(value);
+          }}
+          disablePortal
+          id="combo-box-demo"
+          options={["All Brands", ...brand]}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Brand" />}
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={sortBy}
+            label="sortBy"
+            onChange={handleSelectChange}
+          >
+            <MenuItem value="Rating">Rating</MenuItem>
+            <MenuItem value="Deals">Deals</MenuItem>
+            <MenuItem value="Lowest">Lowest Price</MenuItem>
+            <MenuItem value="Hightest">Hightest Price</MenuItem>
+          </Select>
+        </FormControl>
         <Slider
           getAriaLabel={() => "slider"}
           value={value}
@@ -105,12 +144,6 @@ function App() {
           max={Math.max(...data.map((el) => el.price))}
         />
       </div>
-      <input
-        type="text"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setQuery(e.target.value);
-        }}
-      />
       <Grid container spacing={2} justifyContent="space-around">
         {filteredData.slice((page - 1) * 12, (page - 1) * 12 + 12).map((el) => (
           <Grid item xs={Math.ceil(12 / (window.innerWidth / 300))}>
@@ -150,6 +183,12 @@ function App() {
                 <Typography variant="body1" fontWeight="bold">
                   ${el.price}
                 </Typography>
+                <Typography variant="body1">
+                  {"rating :" + el.rating}
+                </Typography>
+                <Typography variant="body1">
+                  {"discountPercentage :" + el.discountPercentage}
+                </Typography>
               </CardContent>
               <CardActions>
                 <Button size="small">Share</Button>
@@ -168,10 +207,10 @@ function App() {
             .fill(0)
             .map((el, i) => (
               <Button
+                disabled={page === i + 1 ? true : false}
                 onClick={() => {
                   setPage(i + 1);
                 }}
-                sx={{ color: page == i + 1 ? "gray" : "white" }}
               >
                 {i + 1}
               </Button>
